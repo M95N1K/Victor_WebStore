25cc79d82d06fca24b0479ccf1d6db4f812665ea
M95N1K <mysnik999@gmail.com>
2020-10-23 23:05:18 +0600

    Добавление Identity

---
+++ Victor_WebStore.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Victor_WebStore.Domain.Entities
+{
+    public class User : IdentityUser
+    {
+
+    }
+}
--- Victor_WebStore/Startup.cs
+++ Victor_WebStore/Startup.cs
@@ -5,11 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Victor_WebStore.DAL;
+using Victor_WebStore.Domain.Entities;
 using Victor_WebStore.Infrastructure;
 using Victor_WebStore.Infrastructure.Interfaces;
 using Victor_WebStore.Infrastructure.Services;
@@ -35,6 +37,35 @@ namespace Victor_WebStore
 
             services.AddDbContext<WebStoreContext>(options => options
                 .UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<WebStoreContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                //Настройки пароля
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+
+                //
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                options.Lockout.AllowedForNewUsers = true;
+
+                options.User.RequireUniqueEmail = true;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                //options.Cookie.Expiration = TimeSpan.FromDays(150);
+                options.LoginPath = "/Account/Login";
+                options.LogoutPath = "/Account/Logout";
+                options.AccessDeniedPath = "/Account/AccessDenaied";
+                options.SlidingExpiration = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,7 +78,7 @@ namespace Victor_WebStore
 
             app.UseStaticFiles();
 
-            var hellp = _configuration["CustomHW"];
+            app.UseAuthentication();
             
             app.UseRouting();
 
@@ -56,9 +87,6 @@ namespace Victor_WebStore
                 endpoints.MapControllerRoute(
                     "default", 
                     "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapControllerRoute(
-                    "default",
-                    "{controller=Products}/{action=All}/{id?}");
 
                 endpoints.MapGet("/", async context =>
                 {
