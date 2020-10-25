a623a1e83d199a0cd76bba7711b84fa475f2cc5e
M95N1K <mysnik999@gmail.com>
2020-10-25 23:33:55 +0600

    fix

--- Victor_WebStore/Startup.cs
+++ Victor_WebStore/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -65,17 +66,18 @@ namespace Victor_WebStore
                 options.LogoutPath = "/Account/Logout";
                 options.AccessDeniedPath = "/Account/AccessDenaied";
                 options.SlidingExpiration = true;
+
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseStaticFiles();
 
             app.UseAuthentication();
