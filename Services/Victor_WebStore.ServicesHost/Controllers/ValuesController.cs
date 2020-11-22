6915db0e0c0bb9f5351bd87f76d59309b564d51e
M95N1K <mysnik999@gmail.com>
2020-11-22 23:17:43 +0600

    Реализованно добавление, редактирование и удаление элементов

--- Services/Victor_WebStore.ServicesHost/Controllers/ValuesController.cs
+++ Services/Victor_WebStore.ServicesHost/Controllers/ValuesController.cs
@@ -31,14 +31,14 @@ namespace Victor_WebStore.ServicesHost.Controllers
 
         // POST api/<ValuesController>
         [HttpPost]
-        public void Post(string value) //[FromBody] - привязывает входящие данные jSon к модели класса
+        public void Post([FromBody] string value) //[FromBody] - привязывает входящие данные jSon к модели класса
         {
             _Values.Add(value);
         }
 
         // PUT api/<ValuesController>/5
         [HttpPut("{id}")]
-        public ActionResult Put(int id, string value)
+        public ActionResult Put(int id, [FromBody] string value)
         {
             if (id < 0)
                 return BadRequest();
--- UI/Victor_WebStore/Controllers/WebApiController.cs
+++ UI/Victor_WebStore/Controllers/WebApiController.cs
@@ -11,6 +11,9 @@ namespace Victor_WebStore.Controllers
     {
         private readonly IValueService _valueService;
 
+        //[BindProperty]
+        //public string Value { get; set; }
+
         public WebApiController(IValueService valueService)
         {
             _valueService = valueService;
@@ -20,6 +23,44 @@ namespace Victor_WebStore.Controllers
         {
             var values = _valueService.Get();
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(string value)
+        {
+            _valueService.Post(value);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if(id < 0)
+                return RedirectToAction("NotFound404", "Home");// возвращаем результат 404 Not Found
+            var values = _valueService.Get().ToList();
+            if (id >= values.Count)
+                return RedirectToAction("NotFound404", "Home");// возвращаем результат 404 Not Found
+            string value = values[id];
+            return View("Edit",value);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, string value)
+        {
+            _valueService.Update(id, value);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _valueService.Delete(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
