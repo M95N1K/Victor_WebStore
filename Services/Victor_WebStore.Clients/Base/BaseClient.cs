f5d29441baae0ea84ca740ad1d55eaeb4475bf33
M95N1K <mysnik999@gmail.com>
2020-11-24 15:01:22 +0600

    Доработка базового клиента WebApi

--- Services/Victor_WebStore.Clients/Base/BaseClient.cs
+++ Services/Victor_WebStore.Clients/Base/BaseClient.cs
@@ -3,10 +3,11 @@
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace Victor_WebStore.Clients.Base
 {
-    public abstract class BaseClient
+    public abstract class BaseClient : IDisposable
     {
         protected readonly string _ServiceAddress;
         protected readonly HttpClient _Client;
@@ -22,6 +23,51 @@ namespace Victor_WebStore.Clients.Base
                     Accept = {new MediaTypeWithQualityHeaderValue("application/json")}
                 }
             };
+        }
+
+        #region Get
+        public T Get<T>(string url) => GetAsunc<T>(url).Result;
+        public async Task<T> GetAsunc<T>(string url)
+        {
+            var response = await _Client.GetAsync(url);
+            return await response.EnsureSuccessStatusCode().Content.ReadAsAsync<T>();
+        }
+        #endregion
+
+        #region Post
+        public HttpResponseMessage Pos<T>(string url, T item) => PostAsync<T>(url, item).Result;
+        public async Task<HttpResponseMessage> PostAsync<T>(string url, T item)
+        {
+            var response = await _Client.PostAsJsonAsync(url, item);
+            return response.EnsureSuccessStatusCode();
+        }
+        #endregion
+
+        #region Put
+        public HttpResponseMessage Put<T>(string url, T item) => PutAsync<T>(url, item).Result;
+        public async Task<HttpResponseMessage> PutAsync<T>(string url, T item)
+        {
+            var response = await _Client.PutAsJsonAsync(url, item);
+            return response.EnsureSuccessStatusCode();
+        }
+        #endregion
+
+        #region Delete
+        public HttpResponseMessage Delete<T>(string url) => DeleteAsunc<T>(url).Result;
+        public async Task<HttpResponseMessage> DeleteAsunc<T>(string url)
+        {
+            var response = await _Client.DeleteAsync(url);
+            return response;
+        } 
+        #endregion
+
+        public void Dispose()
+        {
+            
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing) return;
         }
     }
 }
