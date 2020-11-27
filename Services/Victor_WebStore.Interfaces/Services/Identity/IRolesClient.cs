ed2629db2fa7b5258626d0d8e1540f1a72d555d1
M95N1K <mysnik999@gmail.com>
2020-11-27 11:02:48 +0600

    Revert "Создание интерфейсов Пользователей и Ролей"
    This reverts commit 17bc8f7b5c50d7614144f9facaedd515d64bd79b.

--- Services/Victor_WebStore.Interfaces/Services/Identity/IRolesClient.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Victor_WebStore.Interfaces.Services.Identity
-{
-    public interface IRolesClient : IRoleStore<Role> { }
-}
