17bc8f7b5c50d7614144f9facaedd515d64bd79b
M95N1K <mysnik999@gmail.com>
2020-11-27 11:01:15 +0600

    Создание интерфейсов Пользователей и Ролей

---
+++ Services/Victor_WebStore.Interfaces/Services/Identity/IRolesClient.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Victor_WebStore.Interfaces.Services.Identity
+{
+    public interface IRolesClient : IRoleStore<Role> { }
+}
---
+++ Services/Victor_WebStore.Interfaces/Services/Identity/IUsersClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Victor_WebStore.Interfaces.Services.Identity
+{
+    public interface IUsersClient :
+        IUserRoleStore<User>,
+        IUserPasswordStore<User>,
+        IUserEmailStore<User>,
+        IUserPhoneNumberStore<User>,
+        IUserTwoFactorStore<User>,
+        IUserClaimStore<User>,
+        IUserLoginStore<User>
+    {
+    }
+}
