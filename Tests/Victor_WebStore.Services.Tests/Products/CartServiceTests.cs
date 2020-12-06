fb22f95a439e98a5cc9f2650914945fdd43b0336
M95N1K <mysnik999@gmail.com>
2020-12-06 14:53:22 +0600

    Тесты для методов CartService

--- Tests/Victor_WebStore.Services.Tests/Products/CartServiceTests.cs
+++ Tests/Victor_WebStore.Services.Tests/Products/CartServiceTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Victor_WebStore.Domain.DTO.Order;
 using Victor_WebStore.Domain.DTO.Products;
 using Victor_WebStore.Domain.Entities;
 using Victor_WebStore.Domain.ViewModels;
@@ -18,6 +19,7 @@ namespace Victor_WebStore.Services.Tests.Products
     {
         private Cart _cart;
         private Mock<IProductService> _productServiceMock;
+        private Mock<ICartStore> _cartStoreMock;
         private ICartService _cartService;
 
         [TestInitialize]
@@ -57,6 +59,10 @@ namespace Victor_WebStore.Services.Tests.Products
 
                 });
 
+            _cartStoreMock = new Mock<ICartStore>();
+            _cartStoreMock.Setup(c => c.Cart).Returns(_cart);
+
+            _cartService = new CartService(_productServiceMock.Object,_cartStoreMock.Object);
         }
 
         [TestMethod]
@@ -92,6 +98,109 @@ namespace Victor_WebStore.Services.Tests.Products
 
         }
 
+        [TestMethod]
+        public void CartService_AddToCart_WorkCorrect()
+        {
+            _cart.Items.Clear();
 
+            const int expected_id = 4;
+            const int expected_items_count = 1;
+
+            _cartService.AddToCart(expected_id);
+
+            Assert.Equal(expected_items_count, _cart.ItemsCount);
+            Assert.Single(_cart.Items);
+            Assert.Equal(expected_id, _cart.Items.First().ProductId);
+        }
+
+        [TestMethod]
+        public void CartService_DecrimentFromCart_WorkCorrect()
+        {
+            const int expected_id = 2;
+            const int expected_quantity = 2;
+
+            _cartService.DecrimentFromCart(expected_id);
+
+            var actual_quantity = _cart.Items.FirstOrDefault(c => c.ProductId == expected_id).Quantity;
+
+            Assert.Equal(expected_quantity,actual_quantity);
+        }
+
+        [TestMethod]
+        public void CartService_IncrimentFromCart_WorkCorrect()
+        {
+            const int expected_id = 1;
+            const int expected_quantity = 2;
+
+            _cartService.IncrimentFromCart(expected_id);
+
+            var actual_quantity = _cart.Items.FirstOrDefault(c => c.ProductId == expected_id).Quantity;
+
+            Assert.Equal(expected_quantity, actual_quantity);
+        }
+
+        [TestMethod]
+        public void CartService_RemoveAll_WorkCorrect()
+        {
+            const int expected_items_count = 0;
+
+            _cartService.RemoveAll();
+
+            var actual_items_count = _cart.Items.Count;
+
+            Assert.Equal(expected_items_count, actual_items_count);
+        }
+
+        [TestMethod]
+        public void CartService_RemoveFromCart_WorkCorrect()
+        {
+            const int expected_items_count = 1;
+            const int expected_id = 2;
+            const int remove_id = 1;
+
+            _cartService.RemoveFromCart(remove_id);
+
+            var actual_items_count = _cart.Items.Count;
+            var actual_id = _cart.Items.First().ProductId;
+
+            Assert.Equal(expected_items_count, actual_items_count);
+            Assert.Equal(expected_id, actual_id);
+        }
+
+        [TestMethod]
+        public void CartService_TransformCart_Returns_CartViewModel()
+        {
+            const int expected_items_count = 2;
+
+            var result = _cartService.TransformCart();
+            var actual_items_count = result.Items.Count;
+            Assert.IsType<CartViewModel>(result);
+            Assert.Equal(expected_items_count, actual_items_count);
+        }
+
+        [TestMethod]
+        public void CartService_ToOrderItems_Returns_IEnumerableOrderItemDTO()
+        {
+            const int expected_items_count = 2;
+
+            var result = _cartService.ToOrderItems();
+            var actual_items_count = result.Count();
+
+            Assert.IsType<List<OrderItemDTO>>(result.ToList());
+            Assert.Equal(expected_items_count, actual_items_count);
+        }
+
+        [TestMethod]
+        public void CartService_SetQuantityFromCart_Work()
+        {
+            const int expected_id = 1;
+            const int expected_quantity = 2;
+
+            _cartService.SetQuantityFromCart(expected_id, expected_quantity);
+
+            var actual_quantity = _cart.Items.FirstOrDefault(c => c.ProductId == expected_id).Quantity;
+
+            Assert.Equal(expected_quantity, actual_quantity);
+        }
     }
 }
