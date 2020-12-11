79b6bc75f89559eb74c16c73614fe98876de3925
M95N1K <mysnik999@gmail.com>
2020-12-11 12:14:44 +0600

    Добавление визуального компонента TopCart выводящий счетчик при наличии товара в карзине.

---
+++ UI/Victor_WebStore/ViewComponents/TopCartViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Victor_WebStore.Interfaces.Services;
+
+namespace Victor_WebStore.ViewComponents
+{
+    public class TopCartViewComponent : ViewComponent
+    {
+        private readonly ICartService _cartService;
+
+        public TopCartViewComponent(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public IViewComponentResult Invoke() => View(_cartService.TransformCart());
+    }
+}
